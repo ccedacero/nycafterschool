533c93f741f5eb7d5d584c24999b18175b8c058f
ccedacero <cristian.cedacero@gmail.com>
2020-06-13 22:10:06 -0400

    create controllers

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,2 +1,26 @@
 class EnrollmentsController < ApplicationController
+    def index 
+        @enrollment = Enrollment.all 
+    end
+
+    def new 
+    @enrollment = Enrollment.new 
+    @users = User.all 
+    @programs = Program.all 
+    end 
+
+    def create
+    enrollment = Enrollment.create(enrollment_params)
+    redirect_to enrollment_path(enrollment) 
+    end
+    
+    def show 
+     @enrollment = Enrollment.find(params[:id])
+    end
+    
+    private 
+    def enrollment_params(*args)
+        params.require(:enrollment).permit!
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'open-uri'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create(name:"User 1", email:"email@gmail.com", password:"123abc")
+# User.create(name:"User 2", email:"email@gmail.com", password:"1232bc")
+
+# p "seeing"
+DATA = "https://data.cityofnewyork.us/resource/uvks-tn5n.json"
+
+def get_programs
+    uri = URI.parse(DATA)
+    response = Net::HTTP.get_response(uri)
+    response.body
+    # binding.pry
+    JSON.parse(response.body)
+end
+
+
+ data = get_programs()
+
+ data.each do |dt|
+    # byebug
+    dt[:program]
+    Program.create(program: dt["program"], site_name: dt["site_name"], borough_community: dt["borough_community"],grade_level_age_group: dt["grade_level_age_group"],number_and_street_address: dt["number_and_street_address"])
+ end
